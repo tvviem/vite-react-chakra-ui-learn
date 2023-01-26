f5443173a0a684cec7bd3976783b1ccf60e23a56
tvviem <trieuvinhviem@gmail.com>
2023-01-26 22:07:26 +0700

    2 - Navbar with Flex layout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const RootLayout = () => {
   return (
-    <>
+    <div>
+      <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 };
 export default RootLayout;
