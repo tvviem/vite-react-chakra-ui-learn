f631fbd879cd06e76221e76f05b9583265d9bec2
tvviem <trieuvinhviem@gmail.com>
2023-01-27 20:20:23 +0700

    3 - Sidebar and responsive content with Grid, GridItem, SimpleGrid Layout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,12 +1,24 @@
+import { Grid, GridItem } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 const RootLayout = () => {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      <GridItem
+        as="aside"
+        colSpan={1}
+        bg="purple.400"
+        minHeight="100vh"
+        padding="30px"
+      >
+        <span>SIDE-BAR</span>
+      </GridItem>
+      <GridItem as="main" colSpan={5} padding="10px">
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 };
 export default RootLayout;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,42 +1,23 @@
-import { Box, Container, Heading, Text } from '@chakra-ui/react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
 
 const Dashboard = () => {
-  const boxStyles = {
-    p: '10px',
-    bg: 'purple.400',
-    color: 'white',
-    m: '10px',
-    textAlign: 'center',
-    filter: 'blur(1px)',
-    ':hover': {
-      color: 'black',
-      bg: 'blue.200'
-    }
-  };
   return (
-    <Container as="section" maxWidth="4xl">
-      <Heading my="30px" p="10px">
-        ChakraUI - Heading component
-      </Heading>
-      <Text marginLeft={30}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-        tempora.
-      </Text>
-      <Text ml={30} color="blue.300" fontWeight="bold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-        tempora.
-      </Text>
+    <SimpleGrid padding="10px" spacing="10px" minChildWidth="180px">
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-      <Box my={30} p="20px" bg="orange.400" borderRadius="xl">
-        <Text color="white">
-          This is a Box. Box is the most abstract component on top of which all
-          other Chakra UI components are built. By default, it renders a `div`
-          element
-        </Text>
-      </Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-      <Box sx={boxStyles}>Hello, Ninjas!</Box>
-    </Container>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+    </SimpleGrid>
   );
 };
 
