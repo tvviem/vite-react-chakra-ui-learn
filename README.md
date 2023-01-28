e86b16cacf7500d5478141ab6aefc145699c2219
tvviem <trieuvinhviem@gmail.com>
2023-01-28 21:32:11 +0700

    7 - Form component basic

--- README.md
+++ README.md
@@ -6,8 +6,12 @@
 4. Responsive styles
 5. Lists list icons
 6. Card component
+7. Tabs component
+8. FormControl component
 
 > REF youtube: [short-course](https://www.youtube.com/playlist?list=PL4cUxeGkcC9hcnIeryurNMMcGBHp7AYlP)
 > Navigate: react-router-dom
 > Layout: RootLayout.tsx, Create.tsx, Dashboard.tsx, Profile.tsx
-> Data: db.json
+> Data: db.json (require install json-server) 
+### Run server to response json for dev 
+`json-server -w <path_to_db_json_file>`
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Textarea
+} from '@chakra-ui/react';
+import { Form, useNavigate } from 'react-router-dom';
 
-const Create = () => {
-  return <div>Create</div>;
-};
+// type Task = {
+//   title: string;
+//   description: string;
+//   isPriority: boolean;
+// };
 
-export default Create;
+export default function Create() {
+  let navigate = useNavigate();
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const task = {
+      title: formData.get('title'),
+      description: formData.get('description'),
+      isPriority: formData.get('isPriority') === ''
+    };
+
+    console.log(task);
+    navigate('/');
+  }
+
+  return (
+    <Box maxW="480px">
+      <Form method="post" action="/create" onSubmit={handleSubmit}>
+        <FormControl isRequired mb="40px">
+          <FormLabel>Task name: </FormLabel>
+          <Input type="text" name="title" />
+          <FormHelperText>Enter a descriptive task name</FormHelperText>
+        </FormControl>
+
+        <FormControl mb="40px">
+          <FormLabel>Task description: </FormLabel>
+          <Textarea
+            placeholder="Enter a detailed description for task..."
+            name="description"
+          ></Textarea>
+        </FormControl>
+
+        <FormControl display="flex" alignItems="center" mb="40px">
+          <Checkbox name="isPriority" size="lg" color={'purple'} />
+          <FormLabel mb="0px" ml="10px">
+            Make this a priority task.
+          </FormLabel>
+        </FormControl>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Box>
+  );
+}
